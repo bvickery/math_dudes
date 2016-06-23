cf807bae93d219f52a692029a4b3346783f69f78
Brandon Vickery <bvickery96@hotmail.com>
2016-06-22 18:48:13 -0700

    got any more them define statements?

---
+++ maths.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define put_sheep_in_pen for
+#define no_sheep 0
+#define how_many_sheep_i_have 500
+#define none 0
+int main(){
+//counting sheep
+int sheep_in_pen;
+int rocks = none;
+put_sheep_in_pen(sheep_in_pen = none;sheep_in_pen < how_many_sheep_i_have; sheep_in_pen++){
+	rocks++;
+}
+printf("There are this many sheeps in pen %d",rocks);
+//pythagarous
+//euclid
+//euler
+//gauss
+//lagrange
+	return 0;
+}

a7a3743dd09275b9ab28d05e30c843ddd47d4132
Brandon Vickery <bvickery96@hotmail.com>
2016-06-23 17:47:02 -0700

    added first function

--- maths.c
+++ maths.c
@@ -1,20 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "int_list.h"
+
 #define put_sheep_in_pen for
+#define take_sheep_out_of_pen for
 #define no_sheep 0
 #define how_many_sheep_i_have 500
 #define none 0
-int main(){
+
 //counting sheep
-int sheep_in_pen;
-int rocks = none;
-put_sheep_in_pen(sheep_in_pen = none;sheep_in_pen < how_many_sheep_i_have; sheep_in_pen++){
-	rocks++;
+void what_are_numbers(){
+	int sheep_in_pen;
+	int rocks_in_basket = none;
+	put_sheep_in_pen(sheep_in_pen = none;sheep_in_pen < how_many_sheep_i_have; sheep_in_pen++){
+		rocks_in_basket++;
+	}
+	printf("aww yuss all them sheep be in the pen\n");
+	take_sheep_out_of_pen(rocks_in_basket; rocks_in_basket > none; rocks_in_basket--){
+		sheep_in_pen--;
+	}
+	printf("now we got all them sheep out of the pen for the night");
 }
-printf("There are this many sheeps in pen %d",rocks);
-//pythagarous
+//pythagoras
+//root 2 is irrational
+//his theorem
+void pythagoras_the_lieragoras(){
+	//need some of that python for this one
+}
+
 //euclid
+//the infinitude of primes
+//for this one could mildy change my stringlist so it is now an intlist and then use that store all the numbers, and each new number just divide it by all other numbers in the list and if any of them evenly go in its a lie
+//sum of angles of a triangle
+//Isosceles Triangle Theorem
+//gcd theorem
+//The Perfect Number Theorem
+//The Fundamental Theorem of Arithmetic
+void my_man_euclid(){
+	
+}
+
 //euler
+//basel problem
+//Konigsberg Bridges Problem
+//e
+void I_know_how_to_cross_bridges(){
+	
+}
+
 //gauss
-//lagrange
+//Fundamental Theorem of Algebra
+//sum of all integers
+void do_you_even_sum_bro(){
+	
+}
+
+
+
+
+
+
+
+int main(){
+	what_are_numbers();
+	pythagoras_the_lieragoras();
+	my_man_euclid();
+	I_know_how_to_cross_bridges();
+	do_you_even_sum_bro();
 	return 0;
 }
